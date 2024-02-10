02a528c0e7c239fe8f33fc27ad11287869676e47
Jisan Khan Sobuj <coderboysobuj@gmail.com>
2024-02-11 00:58:17 +0600

    auto fill with fake data in 1st page

---
+++ src/constants/data.ts
@@ -0,0 +1,26 @@
+export const data: any = {
+  firstName: "Sobuj",
+  middleName: "Vai",
+  lastName: "Developer",
+  suffix: "II",
+  dateOfBirth: {
+    day: "3",
+    month: "January",
+    year: "2000",
+  },
+  ssn: "777777",
+  isHomeless: false,
+  address: {
+    street: "Ghior",
+    zip: "10008",
+    city: "dhaka",
+  },
+  isAlsoMailing: true,
+  mailingAddress: {
+    street: "Ghior",
+    zip: "10008",
+    city: "dhaka",
+  },
+  phoneNumber: "018XXXXXXXX",
+  emailAddress: "coder@gmail.com",
+};
--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,1 +1,31 @@
-console.log("hello");
+import { setValue } from "../utils/functions";
+
+import { data } from "../constants/data";
+
+async function main() {
+  const startButton = createButton();
+  document.body.appendChild(startButton);
+  startButton.addEventListener("click", startProcessing);
+}
+
+function startProcessing() {
+  const inputsInPage = document.querySelectorAll("input");
+  const selectInPage = document.querySelectorAll("select");
+  inputsInPage.forEach((input) => {
+    setValue(input, data[input.name]);
+  });
+
+  selectInPage.forEach((select) => {
+    setValue(select, data[select.name]);
+  });
+}
+
+function createButton() {
+  const btnEl = document.createElement("button");
+  btnEl.classList.add("__simple__gov__auto__fill__button");
+  btnEl.textContent = "🔋";
+
+  return btnEl;
+}
+
+main();
